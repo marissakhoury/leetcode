d8fe133ae25ac351cf5e2481e48dee9e8c029dbe
--global <marissakhoury@gmail.com>
2024-04-02 12:36:23 -0700

    added solution to prob 13

---
+++ solutions/13.py
@@ -0,0 +1,28 @@
+"""
+Problem Link: https://leetcode.com/problems/roman-to-integer/
+"""
+
+def roman_to_int(s):
+    """
+    Given a roman numeral, convert it to an integer.
+    Largest to smallest: add them up
+    smaller before larger: subtract smaller 
+    """
+    rom_to_int = { "I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000 }
+    res = 0
+    for i in range(len(s)):
+        # check if i+1 is in bounds and if value at i+1 is greater than value at i
+        if i + 1 < len(s) and rom_to_int[s[i+1]] > rom_to_int[s[i]]:
+            res -= rom_to_int[s[i]] # subtract smaller
+        else:
+            res += rom_to_int[s[i]]
+    return res
+
+if __name__ == "__main__":
+    assert roman_to_int("III") == 3
+    assert roman_to_int("IV") == 4
+    assert roman_to_int("IX") == 9
+    assert roman_to_int("LVIII") == 58
+    assert roman_to_int("MCMXCIV") == 1994
+    print("All tests passed!")
+
