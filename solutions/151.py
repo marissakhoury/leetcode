53e8a9ea4ea5b2dfd0648679a9ff388e2cdc52e1
--global <marissakhoury@gmail.com>
2024-04-06 15:15:05 -0700

    update solution to 151

--- solutions/151.py
+++ solutions/151.py
@@ -8,7 +8,28 @@ def reverse_words_in_string(s):
     s = ' '.join(s)
     return s
 
+import collections
+def reverse_words_in_string_2(s):
+    string_builder = collections.deque()
+    start = -1
+    i = 0
+
+    while i < len(s):
+        if s[i] != " ":
+            start = i
+            while i < len(s) and s[i] != " ":
+                i += 1
+            string_builder.appendleft(s[start: i])
+            i -= 1
+        i += 1
+    return " ".join(string_builder)
 
 if __name__ == "__main__":
-    assert reverse_words_in_string("hello  my name is marissa ") == "marissa is name my hello"
-    print('passed!')
+    assert reverse_words_in_string_2("hello") == "hello"
+    assert reverse_words_in_string_2("hello world") == "world hello"
+    assert reverse_words_in_string_2("  hello world  ") == "world hello"
+    assert reverse_words_in_string_2("hello   world") == "world hello"
+    assert reverse_words_in_string_2("  hello   world   from   python  ") == "python from world hello"
+    assert reverse_words_in_string_2("") == ""
+    assert reverse_words_in_string_2("     ") == ""
+    print("All tests passed!")
